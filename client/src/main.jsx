6b869773cd2675da930f316d5e698d468ac89d92
Fernando Dorneles <fernandodorneles95@gmail.com>
2025-04-21 09:06:54 -0300

    baxkup21042025

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Home from './service/pages/Home/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Home />
   </StrictMode>,
 )
